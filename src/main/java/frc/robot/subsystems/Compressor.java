93c2e9a7e59bebcdf0b998554fe838a648e0e940
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-02-27 16:31:33 -0600

    literally copied from last year's compressor

--- src/main/java/frc/robot/subsystems/Compressor.java
+++ src/main/java/frc/robot/subsystems/Compressor.java
@@ -4,14 +4,54 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
+/** The subsystem for the compressor */
 public class Compressor extends SubsystemBase {
+  private Relay compressorRelay;
+  private DigitalInput pressureSwitch;
+
   /** Creates a new Compressor. */
-  public Compressor() {}
+  public Compressor() {
+    this.compressorRelay = new Relay(Constants.Compressor.COMPRESSOR_RELAY_PORT);
+    this.pressureSwitch = new DigitalInput(Constants.Compressor.PRESSURE_SWITCH_DIO_PORT);
+  }
+
+  /**
+   * Activates the compressor.
+   *
+   * @return If the compressor was able to be turned on.
+   */
+  public boolean activateCompressor() {
+    if (!this.pressureSwitch.get()) {
+      this.compressorRelay.set(Value.kForward);
+      return true;
+    } else {
+      DriverStation.reportWarning(
+          "Warning: Attempted to activate compressor while pressure switch activated", false);
+      return false;
+    }
+  }
+
+  /** Disables the compressor. */
+  public void disableCompressor() {
+    this.compressorRelay.set(Relay.Value.kOff);
+  }
+
+  /**
+   * Returns true if the compressor is at pressure. False otherwise.
+   *
+   * @return true if the compressor is at pressure. False otherwise.
+   */
+  public boolean isAtPressure() {
+    return this.pressureSwitch.get();
+  }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void periodic() {}
 }

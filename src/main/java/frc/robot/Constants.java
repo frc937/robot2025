45e0df26f724cca6118ae13b10167c650600a932
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-02-27 15:47:19 -0600

    add controller relevant constants and javadoc

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,5 +4,25 @@
 
 package frc.robot;
 
-/** Add your docs here. */
-public class Constants {}
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  /** Constants that are relating to the controllers. */
+  public static final class Controllers {
+    /** Driver station port number for the pilot controller */
+    public static final int PILOT_CONTROLLER_PORT = 0;
+
+    /** Driver station port number for the operator controller */
+    public static final int OPERATOR_CONTROLLER_PORT = 2;
+
+    /** Axis deadband for driver controller. */
+    public static final double DRIVER_CONTROLLER_DEADBAND = 0.1;
+  }
+}

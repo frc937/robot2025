c084c55f1d61991cfa07540815035600c218885a
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-02-27 16:14:49 -0600

    configure default controller keybinds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,17 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Controllers.Keymap;
 
 public class RobotContainer {
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+    Controllers.configureKeybinds(Keymap.Default);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");

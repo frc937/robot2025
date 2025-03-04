d271f356719f8e8a867adc37272b5fd0010f0f35
Oliver <3086549@smsd.org>
2025-03-04 15:22:48 -0600

    Add getters for elevator limit switches(and spotless)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,20 +42,21 @@ public final class Constants {
      */
     public static final int COMPRESSOR_PRESSURE_SWITCH_DEADBAND = 50; /* 1 second */
   }
-  /** The constants for the Elevator  */
-  public static final class Elevator  {
+
+  /** The constants for the Elevator */
+  public static final class Elevator {
 
     /** Motor ID for the left elevator motor */
     public static final int LEFT_ELEVATOR_MOTOR_ID = 0;
 
     /** Motor ID for the left elevator motor */
-    public static final int RIGHT_ELEVATOR_MOTOR_ID = 0;  
+    public static final int RIGHT_ELEVATOR_MOTOR_ID = 0;
 
-    /** Dio port Id for the top elevator limit switch*/
-    public static final int ELEVATOR_TOP_LIMIT_SWITCH_DIO_PORT =0;
-    
+    /** Dio port Id for the top elevator limit switch */
+    public static final int ELEVATOR_TOP_LIMIT_SWITCH_DIO_PORT = 0;
+
     /** DIO port ID for the bottom elevator limit switch */
-    public static final int ELEVATOR_BOTTOM_LIMIT_SWITCH_DIO_PORT =0;
+    public static final int ELEVATOR_BOTTOM_LIMIT_SWITCH_DIO_PORT = 0;
 
     /** Inversion state of the right elevator motor */
     public static final boolean RIGHT_ELEVATOR_MOTOR_INVERTED = false;
@@ -72,5 +73,4 @@ public final class Constants {
     /** Idle mode for the elevator motors.(Either break or coast) */
     public static final IdleMode ELEVATOR_MOTOR_IDLE_MODE = IdleMode.kBrake;
   }
-
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,15 +4,15 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
 /** Elevator of the robot */
 public class Elevator extends SubsystemBase {
 
@@ -25,37 +25,64 @@ public class Elevator extends SubsystemBase {
   public Elevator() {
 
     this.leftMotor = new SparkMax(Constants.Elevator.LEFT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
-    this.rightMotor = new SparkMax(Constants.Elevator.RIGHT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
+    this.rightMotor =
+        new SparkMax(Constants.Elevator.RIGHT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
     this.topLimitSwitch = new DigitalInput(Constants.Elevator.ELEVATOR_TOP_LIMIT_SWITCH_DIO_PORT);
-    this.bottomLimitSwitch = new DigitalInput(Constants.Elevator.ELEVATOR_BOTTOM_LIMIT_SWITCH_DIO_PORT); 
+    this.bottomLimitSwitch =
+        new DigitalInput(Constants.Elevator.ELEVATOR_BOTTOM_LIMIT_SWITCH_DIO_PORT);
   }
 
-private void configureMotors() {
-  SparkMaxConfig generalConfig = new SparkMaxConfig();
-  generalConfig.idleMode(Constants.Elevator.ELEVATOR_MOTOR_IDLE_MODE);
-  generalConfig.smartCurrentLimit(Constants.Elevator.ELEVATOR_MOTOR_CURRENT_LIMIT);
+  private void configureMotors() {
+    SparkMaxConfig generalConfig = new SparkMaxConfig();
+    generalConfig.idleMode(Constants.Elevator.ELEVATOR_MOTOR_IDLE_MODE);
+    generalConfig.smartCurrentLimit(Constants.Elevator.ELEVATOR_MOTOR_CURRENT_LIMIT);
 
-  SparkMaxConfig leftMotorConfig = new SparkMaxConfig().apply(generalConfig);
-  SparkMaxConfig rightMotorConfig = new SparkMaxConfig().apply(generalConfig);
+    SparkMaxConfig leftMotorConfig = new SparkMaxConfig().apply(generalConfig);
+    SparkMaxConfig rightMotorConfig = new SparkMaxConfig().apply(generalConfig);
 
-  leftMotorConfig.follow(this.rightMotor , Constants.Elevator.ELEVATOR_FOLLOWER_INVERSE_STATE);
-  rightMotorConfig.inverted(Constants.Elevator.RIGHT_ELEVATOR_MOTOR_INVERTED);
+    leftMotorConfig.follow(this.rightMotor, Constants.Elevator.ELEVATOR_FOLLOWER_INVERSE_STATE);
+    rightMotorConfig.inverted(Constants.Elevator.RIGHT_ELEVATOR_MOTOR_INVERTED);
 
+    this.leftMotor.configure(
+        leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    this.rightMotor.configure(
+        rightMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+  }
 
-  this.leftMotor.configure(leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-  this.rightMotor.configure(rightMotorConfig,ResetMode.kResetSafeParameters,PersistMode.kNoPersistParameters);
-}
-/** Runs the Elevator motor */
-public void extendElevator( ) {
-  
-  rightMotor.set(Constants.Elevator.ELEVATOR_MOTOR_SPEED);
+  /** Runs the Elevator motor */
+  public void extendElevator() {
 
-}
-/** Runs the elevator motors in reverse */
-public void retractElevator(){
+    rightMotor.set(Constants.Elevator.ELEVATOR_MOTOR_SPEED);
+  }
 
-  rightMotor.set(-Constants.Elevator.ELEVATOR_MOTOR_SPEED);
-}
+  /** Runs the elevator motors in reverse */
+  public void retractElevator() {
+
+    rightMotor.set(-Constants.Elevator.ELEVATOR_MOTOR_SPEED);
+  }
+
+  /**
+   * Returns a Boolean Value that is whether or not the extension limit switch(for the elevator) has
+   * been activated.
+   *
+   * @return the status of the top limit switch
+   */
+  public boolean getTopLimitSwitch() {
+    /*assumes the limit switch is wired to be closed */
+    return !topLimitSwitch.get();
+  }
+
+  /**
+   * Returns a Boolean Value that is whether or not the retraction limit switch(for the elevator)
+   * has been activated.
+   *
+   * @return the status of the bottom limit switch
+   */
+  public boolean getBottomLimitSwitch() {
+    /*assumes the limit switch is wired to be closed */
+    return !bottomLimitSwitch.get();
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run

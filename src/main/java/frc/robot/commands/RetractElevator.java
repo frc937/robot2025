c043342b986065e0774eecc1ed5d92b149e10225
Oliver <3086549@smsd.org>
2025-03-04 16:08:00 -0600

    Add functionality to retract elevator

--- src/main/java/frc/robot/commands/RetractElevator.java
+++ src/main/java/frc/robot/commands/RetractElevator.java
@@ -22,7 +22,10 @@ public class RetractElevator extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+    this.elevator.retractElevator();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -30,11 +33,15 @@ public class RetractElevator extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+    this.elevator.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return elevator.getBottomLimitSwitch();
+    
   }
 }

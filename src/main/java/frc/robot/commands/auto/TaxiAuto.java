3c5799223ec6ef0e856117bacdb370b504554679
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2025-04-16 11:18:35 -0500

    spotless

--- src/main/java/frc/robot/commands/auto/TaxiAuto.java
+++ src/main/java/frc/robot/commands/auto/TaxiAuto.java
@@ -16,6 +16,8 @@ public class TaxiAuto extends ParallelDeadlineGroup {
   /** Creates a new TaxiAuto. */
   public TaxiAuto() {
     super(new WaitCommand(Constants.Auto.TAXI_WAIT_DURATION));
-    addCommands(new DriveAutoRobotOriented(-Constants.Auto.TAXI_AUTO_SPEED, Constants.Auto.TAXI_AUTO_ROTATION));
+    addCommands(
+        new DriveAutoRobotOriented(
+            -Constants.Auto.TAXI_AUTO_SPEED, Constants.Auto.TAXI_AUTO_ROTATION));
   }
 }
